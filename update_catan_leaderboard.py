993472bde11bb82c0770ee2b544301dbce1aaff2
Jad Shaheen <shaheenjad@gmail.com>
2024-08-20 11:10:34 -0400

    Removing unused import

--- update_catan_leaderboard.py
+++ update_catan_leaderboard.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
