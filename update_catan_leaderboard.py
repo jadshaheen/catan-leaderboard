e37b5a9391e515036c5e3ad5c5a6a4f885691c62
Jad Shaheen <shaheenjad@gmail.com>
2025-01-20 12:37:45 -0500

    Fixing chromedriver

--- update_catan_leaderboard.py
+++ update_catan_leaderboard.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
@@ -47,7 +46,7 @@ def get_game_history_table(player):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(service=Service(CHROME_DRIVER_PATH), options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
     table_rows = None
 
